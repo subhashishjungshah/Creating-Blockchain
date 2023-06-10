8436344097aca9b04c5e5b7e1dc6a0f33c3c72c8
subhashishshah <subhashish@ismt.edu.np>
2023-06-10 08:21:50 +0545

    Added a genesis block for our blockchain

--- block.js
+++ block.js
@@ -1,21 +1,26 @@
-class Block{
-	
-	constructor({ timeStamp, preHash, hash, data }){
-		this.timeStamp = timeStamp;
-		this.preHash = preHash;
-		this.hash = hash;
-		this.data = data;		
-	}
-	
+const GENESIS_BLOCK = require("./config");
+
+class Block {
+  constructor({ timeStamp, preHash, hash, data }) {
+    this.timeStamp = timeStamp;
+    this.preHash = preHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  static genesis() {
+    return new this(GENESIS_BLOCK);
+  }
 }
 
-demo_block = new Block({
+const demo_block = new Block({
+  timeStamp: "04/04/200",
+  timeStamp: "04/04/200",
+  preHash: "0xcac",
+  hash: "0xca",
+  data: "Hello Subahshish",
+});
 
-	timeStamp: '04/04/200',
-	preHash: '0xcac',
-	hash: '0xca',
-	data: 'Hello Subahshish',
+const genesis = Block.genesis();
 
-})
-
-console.log(demo_block);
+console.log(genesis);
---
+++ config.js
@@ -0,0 +1,11 @@
+const GENESIS_BLOCK = {
+  timeStamp: 1,
+
+  preHash: "0x000",
+
+  hash: "0x123",
+
+  data: [],
+};
+
+module.exports = GENESIS_BLOCK;
