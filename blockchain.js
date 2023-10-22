c8f6ef55ad048466a6f7b708d9426f6694a9b898
subhashishjungshah <subhashishjungshah@gmail.com>
2023-10-22 20:01:16 +0545

    added longest chain principle

--- blockchain.js
+++ blockchain.js
@@ -12,17 +12,34 @@ class Blockchain {
     });
     this.chain.push(newBlock);
   }
+  // Validate function for blockchain
   static isValidateChain(chain) {
+    // Check whether first block is zenesis or not
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
       return false;
+
     for (let index = 1; index < chain.length; index++) {
+      // Checking whether the previous hash and chain hash matches or not
       const { timeStamp, prevHash, hash, data } = chain[index];
       if (prevHash !== chain[index - 1].hash) return false;
 
+      // checks whether the blocked hash matches the algorithm's hash or not
       const verifiedHash = cryptoHash(timeStamp, prevHash, data);
       if (verifiedHash !== hash) return false;
     }
     return true;
+  }
+  // Longest chain prinicple
+  replaceChain(chain) {
+    if (chain <= this.chain.length) {
+      console.error("The income chain isn't longer");
+      return;
+    }
+    if (!this.isValidateChain(chain)) {
+      console.error("The incoming chain isn't valid");
+      return;
+    }
+    this.chain = chain;
   }
 }
 const blockchain = new Blockchain();
